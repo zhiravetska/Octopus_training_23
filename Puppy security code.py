bbc95ca81651a667a2b508389772f2a87bb0cb09
Anastasia <anastasija.ziravecka@rtu.lv>
2023-02-02 20:49:41 +0200

    new_code

---
+++ Puppy security code.py
@@ -0,0 +1,12 @@
+def create_permitations(numpad_list):
+    def permutation_looper(first_number_index = 0):
+        n = len(numpad_list)
+
+
+    return(permutation_output)
+
+
+
+
+
+create_permitations([1,2,3])
