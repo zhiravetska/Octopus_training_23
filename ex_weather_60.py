775fe02c61adcc0884e7b7f86b31fb62e6ba26f6
Anastasia <anastasija.ziravecka@rtu.lv>
2023-05-03 11:22:11 +0300

    smthnew

--- ex_weather_60.py
+++ ex_weather_60.py
@@ -1,10 +1,21 @@
-import re
-import re
-import urllib
-city = input("Enter your city: ")
+#import re
+#import re
+#import urllib
+#city = input("Enter your city: ")
 # https://www.weather-forecast.com/locations/Seattle/forecasts/latest
-url = "https://www.weather-forecast.com/locations/" + city + "/forecasts/latest"
+#url = "https://www.weather-forecast.com/locations/" + city + "/forecasts/latest"
 
-data = urllib.request.urlopen(url).read()
-data1 = data.decode("utf-8")
-print(data1)
+#data = urllib.request.urlopen(url).read()
+#data1 = data.decode("utf-8")
+#print(data1)
+
+#Nr. 71, weather (no attribute 'find'?????)
+import requests
+from bs4 import *
+data = requests.get("https://www.accuweather.com/en/fr/san-damiano/158758/weather-forecast/158758")
+soup = BeautifulSoup(data.text, "html.parser")
+data2 = soup.find('div', {'class':'info'})
+data3 = data2.find('strong', {'class':'temp'})
+data4 = data3.contents[0]
+
+
